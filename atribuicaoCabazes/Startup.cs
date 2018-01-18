2fa4db69434073a6856919d090a680f162068e8f
Joaquim <16487@alunos.ipbeja.pt>
2018-01-18 16:34:13 +0000

    First commit

---
+++ atribuicaoCabazes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(atribuicaoCabazes.Startup))]
+namespace atribuicaoCabazes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
